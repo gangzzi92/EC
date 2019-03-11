18bb51833719e4354c411a93cbfa435394e9a934
gangzzi92 <7dodhd7@naver.com>
2019-03-11 12:30:37 +0900

    Signed-off-by: gangzzi92 <7dodhd7@naver.com>

--- Test2/src/Test2_1.java
+++ Test2/src/Test2_1.java
@@ -2,15 +2,12 @@ import java.util.Scanner;
 
 public class Test2_1 {
 	public static void main(String[] args) {
-		System.out.println("화면에 숫자를 입력해 주세요!");
 		Scanner s = new Scanner(System.in);
-		int n1 = s.nextInt();
-		System.out.print("입력한 숫자는 ");
-		System.out.println(n1);
-		
-		System.out.println("화면에 숫자를 또 입력해 주세요!");
-		int n2 = s.nextInt();
-		System.out.print("입력한 숫자는 ");
-		System.out.println(n2);
+		System.out.println("첫번째 숫자를 입력해 주세요.");
+		int a = s.nextInt();
+		System.out.println("두번째 숫자를 입력해 주세요.");
+		int b = s.nextInt();
+		int sum = a + b;
+		System.out.printf("%d + %d = %d입니다.", a, b, sum);
 	}
 }
--- Test2/src/Test2_2.java
+++ Test2/src/Test2_2.java
@@ -11,6 +11,7 @@ public class Test2_2 {
 		System.out.println(n1 + n2);
 		System.out.print("첫번째 숫자 + 10 = ");
 		System.out.println(n1 + 10);
-
+		System.out.printf("첫번째 숫자 + 두번째 숫자 = %d\n", n1 + n2);
+		System.out.printf("%d와 %d의 합은 %d입니다.", n1, n2, n1 + n2);
 	}
 }
